def98d46ad63d4c75dd5d052c91b3345a09862f2
Marcio Cruz de Almeida <marciocadev@gmail.com>
2025-12-23 10:29:08 -0300

    add log to api gatewau from stack002

--- lib/cdk-examples-stack.ts
+++ lib/cdk-examples-stack.ts
@@ -1,7 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import { Stack001NestedStack } from './stack001/stack001';
-// import { Stack002NestedStack } from './stack002/stack002';
+import { Stack002NestedStack } from './stack002/stack002';
 // import { Stack003NestedStack } from './stack003/stack003';
 
 export class CdkExamplesStack extends Stack {
@@ -9,7 +9,7 @@ export class CdkExamplesStack extends Stack {
     super(scope, id, props);
 
     new Stack001NestedStack(this, "Stack001", props);
-    // new Stack002NestedStack(this, "Stack002", props);
+    new Stack002NestedStack(this, "Stack002", props);
     // new Stack003NestedStack(this, "Stack003", props);
   }
 }
